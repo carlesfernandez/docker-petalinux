93af72dab74e9c224efbdf8c821c251bcfd38344
Carles Fernandez <carles.fernandez@gmail.com>
2022-08-04 09:49:26 +0200

    Fix errors raised by ShellCheck

--- docker_build.sh
+++ docker_build.sh
@@ -21,13 +21,14 @@ case $PYTHONV in
     3*) PYTHONHTTP="http.server" ;;
     *) PYTHONHTTP="SimpleHTTPServer" ;;
 esac
+# shellcheck disable=SC2009
 if ! ps -fC python | grep "$PYTHONHTTP" > /dev/null ; then
     python -m "$PYTHONHTTP" &
     HTTPID=$!
     echo "HTTP Server started as PID $HTTPID"
-    trap "kill $HTTPID" EXIT KILL QUIT SEGV INT HUP TERM ERR
+    trap 'kill $HTTPID' EXIT QUIT SEGV INT HUP TERM ERR
 fi
 
 echo "Creating Docker image petalinux:$XILVER..."
-time docker build . -t petalinux:$XILVER --build-arg XILVER=${XILVER} --build-arg XXXX_XXXX=${XXXX_XXXX}
+time docker build . -t petalinux:"$XILVER" --build-arg XILVER="${XILVER}" --build-arg XXXX_XXXX="${XXXX_XXXX}"
 [ -n "$HTTPID" ] && kill $HTTPID && echo "Killed HTTP Server"
